1f6af106fbdf09caba89ec61ed752c019e409907
adam <adamkabbara0@gmail.com>
2026-01-20 13:36:44 -0500

    point and point_test run fine

--- labs/lab1/warmup/point.c
+++ labs/lab1/warmup/point.c
@@ -1,23 +1,26 @@
 #include <assert.h>
 #include "common.h"
 #include "point.h"
+#include <math.h>
 
-void
-point_translate(struct point *p, double x, double y)
+void point_translate(struct point *p, double x, double y)
 {
-	TBD();
+	int new_x = x + point_X(p);
+	int new_y = y + point_Y(p); // reminder, eqivanlent to p->y += y 
+	point_set(p, new_x, new_y);
 }
 
-double
-point_distance(const struct point *p1, const struct point *p2)
+double point_distance(const struct point *p1, const struct point *p2)
 {
-	TBD();
-	return -1.0;
+	return sqrt(pow((point_X(p1) - point_X(p2)),2) + pow((point_Y(p1) - point_Y(p2)),2));
 }
 
-int
-point_compare(const struct point *p1, const struct point *p2)
+int point_compare(const struct point *p1, const struct point *p2)
 {
-	TBD();
-	return 0;
+	double dist1 = sqrt(pow(point_X(p1), 2) + pow(point_Y(p1), 2));
+	double dist2 = sqrt(pow(point_X(p2), 2) + pow(point_Y(p2), 2));
+
+	if (dist1 == dist2){return 0;}
+	else if (dist1 > dist2){return 1;}
+	else {return -1;}
 }
