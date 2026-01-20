f67608951d18a4558f1ddca5a85a413496f72494
adam <adamkabbara0@gmail.com>
2026-01-20 01:33:41 -0500

    fixed logit error in fact - under rep overflow error

--- labs/lab1/warmup/fact.c
+++ labs/lab1/warmup/fact.c
@@ -23,10 +23,6 @@ int main(int argc, char* argv[])
 	// if decimals, string, or char, return "Huh?
 	int i = 0;
 	while (argv[1][i] != '\0'){
-		if (i >=3){ // catches all string bigger than 2 chars long
-			printf("Huh?\n");
-			return 0;
-		}
 		if (argv[1][i] - '0' > 9 || argv[1][i] - '0' < 0){ // if not 1 to 9 in ascii, then its a decimal, neg, or other string
 			printf("Huh?\n");
 			return 0;
